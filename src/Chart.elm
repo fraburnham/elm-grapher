140b930ec40eb99eb48b0f1d95adcda7dab25964
Frank Burnham <frank@n8git.dev>
2021-12-04 11:21:32 -0500

    First pass at a stupid, error prone csv parser

--- src/Chart.elm
+++ src/Chart.elm
@@ -1,1 +1,14 @@
 -- eventually a place to put the types and fns that will be used to render charts
+-- for now the fns to render a csv as a table
+
+module Chart exposing (..) -- TODO: only expose what is needed to make the intended interface clear!
+
+import Csv exposing (Data, Header, Rows, Row)
+import Html exposing (Html, button, div, text, input)
+import Html.Attributes exposing (placeholder, value)
+import Html.Events exposing (onClick, onInput)
+
+
+render : Data -> Html msg
+render chartData =
+    div [] []
--- src/Main.elm
+++ src/Main.elm
@@ -1,42 +1,75 @@
 module Main exposing (..)
 
 import Browser
+import Chart
+import Csv
+import File exposing (File)
+import File.Select as Select
 import Html exposing (Html, button, div, text, input)
 import Html.Attributes exposing (placeholder, value)
 import Html.Events exposing (onClick, onInput)
+import String
+import Task
 
-main =
-  Browser.sandbox { init = init, update = update, view = view }
 
-type alias Model =
-    { filename : String }
-
-init : Model
-init =
-    { filename = "" }
-      
-type Msg =
-    Change String |
-    Reset
-
--- update fn takes a message and model and returns a model
-update : Msg -> Model -> Model
-update msg model =
-    case msg of
-        Change newString ->
-            { model | filename = newString }
-
-        Reset ->
-            { model | filename = "" }
-          
+-- fix indentation in elm-mode
+-- may need to clobber the indentation settings for other "IDEs"
 -- Need a styleguide or something. The indentation style elm-mode is providing has some magic I don't understand
 -- install elm-format (there is some fuckery w/ npm i -g elm-format)
 
--- view takes a model and returns an Html and a Msg
+
+main =
+    Browser.element
+        { init = init
+        , update = update
+        , view = view
+        , subscriptions = subscriptions
+        }
+
+
+type alias Model =
+    { csvData : Maybe Csv.Data -- This is a maybe so that we can pattern match to display the right stuff
+    }
+
+init : () -> (Model, Cmd Msg)
+init _ =
+    ( Model Nothing, Cmd.none )
+
+
+type Msg
+    = RequestFile
+    | FileSelected File
+    | DataLoaded String
+
+update : Msg -> Model -> (Model, Cmd Msg)
+update msg model =
+    case msg of
+        RequestFile ->
+            ( model
+            , Select.file ["text/csv"] FileSelected
+            )
+
+        FileSelected file ->
+            ( model
+            , Task.perform DataLoaded (File.toString file)
+            )
+
+        DataLoaded data ->
+            ( { model | csvData = Just (Csv.parse data) }
+            , Cmd.none
+            )
+
+
 view : Model -> Html Msg
 view model =
-  div []
-    [ div [] [ text model.filename ]
-    , button [ onClick Reset ] [ text "Reset" ]
-    , input [ placeholder "Input filename", value model.filename, onInput Change ] []
-    ]
+    case model.csvData of
+        Nothing ->
+            button [ onClick RequestFile ] [ text "Load CSV" ]
+
+        Just data ->
+            Chart.render data
+
+
+subscriptions : Model -> Sub Msg
+subscriptions _ =
+    Sub.none

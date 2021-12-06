4c15d51747e7e65484b4f50315954c177d09a85d
Frank Burnham <frank@n8git.dev>
2021-12-06 13:20:53 -0500

    Reduce data based on selected columns

--- src/Chart/Prepare.elm
+++ src/Chart/Prepare.elm
@@ -5,21 +5,36 @@ import List exposing (map, indexedMap, filter, member)
 import Tuple
 import String
 
--- return a Data that has columns whose name isn't in the selectedColumns list removed
--- this will evole as time goes on to handle flipping columns and such for the render (or the render-er will evolve)
 
-column : List String -> Column -> Column
+headerColumn : List String -> String -> Bool
+headerColumn selectedColumns headerColData =
+    member headerColData selectedColumns
+
+header : List String -> Header -> Header
+header selectedColumns headerData =
+    filter (headerColumn selectedColumns) headerData
+
+column : List String -> Column -> Bool
 column selectedColumns columnData =
-    columnData
+    member columnData.name selectedColumns
 
 row : List String -> Row -> Row
 row selectedColumns rowData =
-    rowData
+    -- there should be logic here to update the completeness of a row?
+    -- since we're reducing the row may be complete after this...
+    case rowData of
+        RowData rd ->
+            filter (column selectedColumns) rd
+                |> RowData
+
+        RowIncomplete rd ->
+            filter (column selectedColumns) rd
+                |> RowIncomplete
 
 rows : List String -> Rows -> Rows
 rows selectedColumns rowsData =
-    rowsData
+    map (row selectedColumns) rowsData
 
 data : List String -> Data -> Data
 data selectedColumns csvData =
-    csvData
+    Data (header selectedColumns csvData.header) (rows selectedColumns csvData.rows)

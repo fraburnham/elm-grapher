fded7f0565f73806ca014356b5269fddeb1f2769
Frank Burnham <fraburnham@users.noreply.github.com>
2021-12-18 08:33:41 -0500

    Simplify division code (#14)

--- src/Chart.elm
+++ src/Chart.elm
@@ -4,16 +4,11 @@ import Chart.Prepare exposing (Data, Row, Rows)
 import Chart.Scale as Scale exposing (Bound, Bounds)
 import Html exposing (Html, div)
 import Html.Attributes as HtmlAttr
-import List exposing (map)
+import List exposing (concatMap, map, range)
 import String
 import Svg exposing (circle, line, svg)
 import Svg.Attributes as SvgAttr
 import Tuple exposing (first, second)
-
-
-type DivisionAxis
-    = X
-    | Y
 
 
 row : Row -> Html msg
@@ -31,53 +26,48 @@ rows rowsData =
     map row rowsData
 
 
-division : Bound -> DivisionAxis -> Int -> Html msg
-division bound divAxis offset =
-    -- type doesn't feel like the right way for this
-    -- I think I'm missing a high order fn option...
-    case divAxis of
-        X ->
-            line
-                [ SvgAttr.x1 (String.fromInt offset)
-                , SvgAttr.y1 (String.fromFloat bound.min)
-                , SvgAttr.x2 (String.fromInt offset)
-                , SvgAttr.y2 (String.fromFloat bound.max)
-                , SvgAttr.stroke "lightgrey"
-                , SvgAttr.strokeDasharray "3 8"
-                ]
-                []
-
-        Y ->
-            line
-                [ SvgAttr.x1 (String.fromFloat bound.min)
-                , SvgAttr.y1 (String.fromInt offset)
-                , SvgAttr.x2 (String.fromFloat bound.max)
-                , SvgAttr.y2 (String.fromInt offset)
-                , SvgAttr.stroke "lightgrey"
-                , SvgAttr.strokeDasharray "3 8"
-                ]
-                []
-
-
-makeDivisions : Bound -> DivisionAxis -> Int -> Int -> List (Html msg) -> List (Html msg)
-makeDivisions bound divAxis divSpacing offset divs =
-    if offset >= floor bound.max then
-        divs
-
-    else
-        makeDivisions bound divAxis divSpacing (offset + divSpacing) (division bound divAxis offset :: divs)
+division : Bound -> Int -> Int -> List (Html msg)
+division bound divSpacing divNum =
+    let
+        offset =
+            divSpacing * (divNum + 1)
+    in
+    [ line
+        [ SvgAttr.x1 (String.fromInt offset)
+        , SvgAttr.y1 (String.fromFloat bound.min)
+        , SvgAttr.x2 (String.fromInt offset)
+        , SvgAttr.y2 (String.fromFloat bound.max)
+        , SvgAttr.stroke "lightgrey"
+        , SvgAttr.strokeDasharray "3 8"
+        ]
+        []
+    , line
+        [ SvgAttr.x1 (String.fromFloat bound.min)
+        , SvgAttr.y1 (String.fromInt offset)
+        , SvgAttr.x2 (String.fromFloat bound.max)
+        , SvgAttr.y2 (String.fromInt offset)
+        , SvgAttr.stroke "lightgrey"
+        , SvgAttr.strokeDasharray "3 8"
+        ]
+        []
+    ]
 
 
 divisions : Bounds -> Int -> List (Html msg)
 divisions { x, y } divCount =
     let
-        xDivSpacing =
-            round x.max // divCount
+        maxBound =
+            if x.max > y.max then
+                x
 
-        yDivSpacing =
-            round y.max // divCount
+            else
+                y
+
+        divSpacing =
+            round maxBound.max // divCount
     in
-    makeDivisions x X xDivSpacing xDivSpacing [] ++ makeDivisions y Y yDivSpacing yDivSpacing []
+    range 0 (divCount - 1)
+        |> concatMap (division maxBound divSpacing)
 
 
 render : Data -> Html msg
